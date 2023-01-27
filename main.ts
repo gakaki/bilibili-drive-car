1a3b5dd215f839e3b4655c7df9151817ae15b22d
Alex he <alex.y.he.-nd@disney.com>
2023-01-27 14:59:48 +0800

    add all

--- main.ts
+++ main.ts
@@ -4,6 +4,13 @@
 /// <reference lib="dom.asynciterable" />
 /// <reference lib="deno.ns" />
 
+//https://deno.land/x/croner@5.4.1
+import Cron from "https://deno.land/x/croner@5.4.1/src/croner.js";
+
+// Cron("* * * * * *", () => {
+//     console.log("[cron] bilibili Fetch JSON To Redis");
+// });
+
 import { start } from "$fresh/server.ts";
 import manifest from "./fresh.gen.ts";
 
