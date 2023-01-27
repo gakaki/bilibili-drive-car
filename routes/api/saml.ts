134fac1284f2ac5e65ddbd9a632e394936bef160
alex he <gakaki@gmail.com>
2023-01-27 13:23:25 +0800

    add req body

--- routes/api/saml.ts
+++ routes/api/saml.ts
@@ -1,6 +1,32 @@
-import { HandlerContext } from "$fresh/server.ts";
+import { HandlerContext, Handlers } from "$fresh/server.ts";
 
-export const handler = (_req: Request, _ctx: HandlerContext): Response => {
+export const handler: Handlers = {
+  async POST(req: Request, ctx: HandlerContext): Promise<Response> {
+      // const payload = req.body
 
-//   return new Response(body);
+      // if (payload === null) {
+      //   return new Response("Invalid request: body is null");
+      // }
+			// console.log(payload)
+      // const decoder = new TextDecoder()
+      // for await (const chunk of payload) {
+      //   // decode the bytes into a string and print it
+      //   console.log(decoder.decode(chunk))
+      // }
+
+			console.log("Method:", req.method);
+
+			const url = new URL(req.url);
+			console.log("Path:", url.pathname);
+			console.log("Query parameters:", url.searchParams);
+		
+			console.log("Headers:", req.headers);
+		
+			if (req.body) {
+				const body = await req.text();
+				console.log("Body:", body);
+			}
+		
+			return new Response("Hello, World!");
+  }
 };
